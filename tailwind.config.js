a63af42bb9029013fb6ce2f162647f77ae83d447
Sunil <skumar@constellant.com>
2022-04-19 18:18:48 +0530

    alpine

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*{.html,js}"],
+  theme: {
+    colors:{
+      'blue':'#307FC1',
+      'white':'#ffffff',
+      'gray':'#4a44447a',
+      'red':'#FF0000',
+      'green':'#008000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
